1a22f22b4b407a0e64be24584ede4693c335e4e3
lisa-ce <lisachikovore@gmail.com>
2026-02-19 11:08:40 +0200

    added timing

--- script.js
+++ script.js
@@ -2,8 +2,18 @@ window.addEventListener("load", function () {
     const intro = document.getElementById("intro");
     const mainContent = document.getElementById("mainContent");
 
-    setTimeout(() => {
+    // Function to hide intro
+    function showLandingPage() {
         intro.classList.add("hide");
         mainContent.classList.add("show");
-    }, 3000); // 3 seconds
+    }
+
+    // Auto after 30 seconds
+    const timer = setTimeout(showLandingPage, 3000);
+
+    // Click to skip
+    intro.addEventListener("click", function () {
+        clearTimeout(timer); // stop the 30s timer
+        showLandingPage();
+    });
 });
