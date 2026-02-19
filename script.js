48f6e4832102f894f1a1d6ff465dc3fa7bf4e67b
lisa-ce <lisachikovore@gmail.com>
2026-02-19 13:12:05 +0200

    added a hamburger

--- script.js
+++ script.js
@@ -2,18 +2,25 @@ window.addEventListener("load", function () {
     const intro = document.getElementById("intro");
     const mainContent = document.getElementById("mainContent");
 
-    // Function to hide intro
+
     function showLandingPage() {
         intro.classList.add("hide");
         mainContent.classList.add("show");
     }
 
-    // Auto after 30 seconds
+    
     const timer = setTimeout(showLandingPage, 3000);
 
-    // Click to skip
+    
     intro.addEventListener("click", function () {
-        clearTimeout(timer); // stop the 30s timer
+        clearTimeout(timer); 
         showLandingPage();
     });
 });
+
+  const hamburger = document.getElementById("hamburger");
+  const menu = document.getElementById("menu");
+
+  hamburger.addEventListener("click", () => {
+    menu.classList.toggle("active");
+  });
